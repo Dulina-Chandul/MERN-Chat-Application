9e8d24b452314d550aa0a0ebd158a07fb45ffb22
Dulina-Chandul <dulinachandul2006@gmail.com>
2026-02-27 22:01:22 +0530

    Created getMessageByUserIdsHandler controller

--- server/src/controllers/message/message.controller.js
+++ server/src/controllers/message/message.controller.js
@@ -1,3 +1,4 @@
+import Message from "../../models/message/message.model.js";
 import User from "../../models/user/User.model.js";
 
 export const messageController = {
@@ -15,6 +16,31 @@ export const messageController = {
     }
   },
   getChatPartnersHandler: async (req, res) => {},
-  getMessageByUserIdsHandler: async (req, res) => {},
+  getMessageByUserIdsHandler: async (req, res) => {
+    try {
+      const myId = req.user._id;
+      const otherUserId = req.params.id;
+
+      const messages = await Message.find({
+        $or: [
+          {
+            senderId: myId,
+            receiverId: otherUserId,
+          },
+          {
+            senderId: otherUserId,
+            receiverId: myId,
+          },
+        ],
+      });
+
+      return res
+        .status(200)
+        .json({ message: "Messages fetched successfully", messages });
+    } catch (error) {
+      console.log("Error fetching messages:", error.message);
+      return res.status(500).json({ message: "Error fetching messages" });
+    }
+  },
   sendMessageHandler: async (req, res) => {},
 };
