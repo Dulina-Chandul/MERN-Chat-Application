1b61509f183028c27314015c9d314aa3d8787c75
Dulina-Chandul <dulinachandul2006@gmail.com>
2026-02-28 21:09:00 +0530

    Installed daisyUI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ const App = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello World!</h1>
+      <button className="btn btn-primary">Click me</button>
     </div>
   );
 };
