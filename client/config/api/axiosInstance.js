4b9a61536864bea2e4bb7cafbc4863faedf986ed
Dulina-Chandul <dulinachandul2006@gmail.com>
2026-02-28 21:35:04 +0530

    Added gradient background and installed and setup axios

---
+++ client/config/api/axiosInstance.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:3000/api"
+      : "https://domain.com/api",
+  withCredentials: true,
+});
