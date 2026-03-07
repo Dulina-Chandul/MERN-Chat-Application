4e92968ebf411f871be451fef9bd6ec81f7c90a0
Dulina-Chandul <dulinachandul2006@gmail.com>
2026-03-07 23:14:48 +0530

    Sign up page

--- client/src/pages/SignUpPage.jsx
+++ client/src/pages/SignUpPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { useAuthStore } from "../store/useAuthStore";
+import { useAuthStore } from "../store/useAuthStore.js";
+import BorderAnimatedContainer from "../components/BorderAnimatedContainer";
 
 const SignUpPage = () => {
   const [formData, setFormData] = useState({
@@ -8,11 +9,19 @@ const SignUpPage = () => {
     password: "",
   });
 
-  const [signup, isSigningUp] = useAuthStore();
+  // const [signup, isSigningUp] = useAuthStore();
 
   const handleSubmit = (e) => {};
 
-  return <div>SignUpPage</div>;
+  return (
+    <div className="w-full flex items-center justify-center p-4 bg-slate-900">
+      <div className="relative w-full max-w-6xl md:h-[800px] h-[650px]">
+        <BorderAnimatedContainer>
+          <div className="w-full flex flex-col md:flex-row">kasdfsdf</div>
+        </BorderAnimatedContainer>
+      </div>
+    </div>
+  );
 };
 
 export default SignUpPage;
