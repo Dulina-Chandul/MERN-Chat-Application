67666b6511166c870f88d19983306486498bd417
Dulina-Chandul <dulinachandul2006@gmail.com>
2026-03-07 22:50:19 +0530

    Created the basic structure of the signup page

--- client/src/pages/SignUpPage.jsx
+++ client/src/pages/SignUpPage.jsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import { useAuthStore } from "../store/useAuthStore";
 
 const SignUpPage = () => {
+  const [formData, setFormData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  });
+
+  const [signup, isSigningUp] = useAuthStore();
+
+  const handleSubmit = (e) => {};
+
   return <div>SignUpPage</div>;
 };
 
