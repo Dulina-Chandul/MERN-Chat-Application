b5b17abe5e92b35f71d309aba0ccaeaf6aa953b6
Dulina-Chandul <dulinachandul2006@gmail.com>
2026-03-25 13:45:58 +0530

    Added the tabswith functionality

--- client/src/components/ActiveTabSwitch.jsx
+++ client/src/components/ActiveTabSwitch.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { useChatStoe } from "../store/useChatStore";
 
 const ActiveTabSwitch = () => {
-  return <div>ActiveTabSwitch</div>;
+  const { activeTab, setActiveTab } = useChatStoe();
+
+  return (
+    <div className="tabs tabs-boxed bg-transparent p-2 m-2 w-full">
+      <button
+        onClick={() => setActiveTab("chats")}
+        className={`tab flex-1 ${activeTab === "chats" ? "bg-cyan-500/20 text-cyan-400" : "text-slate-400"} rounded-sm`}
+      >
+        Chats
+      </button>
+      <button
+        onClick={() => setActiveTab("contacts")}
+        className={`tab flex-1 ${activeTab === "contacts" ? "bg-cyan-500/20 text-cyan-400" : "text-slate-400"} rounded-sm`}
+      >
+        Contacts
+      </button>
+    </div>
+  );
 };
 
 export default ActiveTabSwitch;
